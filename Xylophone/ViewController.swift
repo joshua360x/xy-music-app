b83009fbb021d3ee2e4a5be3fbf078b2f1c94d13
Josh Williams <joshua360x@gmail.com>
2023-05-08 14:18:00 -0700

    added sound differentiation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("------------------------->")
+        print("this is the title : \(sender.titleLabel?.text))")
+        playSound(soundToPlay: sender.titleLabel?.text ?? "C")
+    }
     
-
+    func playSound(soundToPlay: String) {
+        let url = Bundle.main.url(forResource: soundToPlay, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
